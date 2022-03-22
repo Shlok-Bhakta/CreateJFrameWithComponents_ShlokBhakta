a03ab5041d987554eeb4e65360e531af36355f47
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-22 11:56:08 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrameWithComponents aFrame = new JFrameWithComponents();
+        final int WIDTH = 350;
+        final int HEIGHT = 100;
+        aFrame.setSize(WIDTH, HEIGHT);
+        aFrame.setVisible(true);
+    }
+}
