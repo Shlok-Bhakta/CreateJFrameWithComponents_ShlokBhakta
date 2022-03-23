ad5686855c513bff7693bfaa4e2c5af20400cef5
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-23 12:01:20 -0500

    initcomit

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JAction aFrame = new JAction();
+        JAction2 aFrame = new JAction2();
         final int WIDTH = 250;
         final int HEIGHT = 100;
         aFrame.setSize(WIDTH, HEIGHT);
         aFrame.setVisible(true);
+
     }
 }
--- src/JAction2.java
+++ src/JAction2.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class JAction extends JFrame implements ActionListener{
+public class JAction2 extends JFrame implements ActionListener{
     JLabel label = new JLabel("Enter your name");
     JTextField field = new JTextField(12);
     JButton button = new JButton("OK");
-    public JAction(){
+    public JAction2(){
         super("Frame with components");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
